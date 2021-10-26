b18bd8ecaac0faeceeab59fb748665365ade57e1
homelyseven250 <georgee.keylock@outlook.com>
2021-10-26 08:57:11 +0100

    Use black formatting

--- cogs/music.py
+++ cogs/music.py
@@ -9,12 +9,13 @@ import requests
 
 
 class Music(commands.Cog):
-    ydl = youtube_dl.YoutubeDL({'format': '251'})
+    ydl = youtube_dl.YoutubeDL({"format": "251"})
 
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.queues = {}
         self.bot.loop.create_task(self.__ainit__())
-    
+
     async def __ainit__(self) -> None:
         """
         |coro|
@@ -31,51 +32,109 @@ class Music(commands.Cog):
         close after finishing final iteration.
         """
         return super().cog_unload()
-    
+
     async def getURL(self, song: str):
         page = await self.browser.newPage()
-        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0')
-        await page.goto(f'https://music.youtube.com/search?q={song}', {'waituntil': 'networkidle0'})
-        # await asyncio.sleep(0.75)
-        url = await page.waitForXPath('/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-tabbed-search-results-renderer/div[2]/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[2]/div[2]/ytmusic-responsive-list-item-renderer[1]/div[2]/div[1]/yt-formatted-string/a')
-        await page.screenshot({'path': './screenshot.png', 'fullPage': True})
-        url = await url.getProperty('href')
+        await page.setUserAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0"
+        )
+        await page.goto(
+            f"https://music.youtube.com/search?q={song}", {"waituntil": "networkidle0"}
+        )
+        url = await page.waitForXPath(
+            "/html/body/ytmusic-app/ytmusic-app-layout/div[3]/ytmusic-search-page/ytmusic-tabbed-search-results-renderer/div[2]/ytmusic-section-list-renderer/div[2]/ytmusic-shelf-renderer[1]/div[2]/ytmusic-responsive-list-item-renderer/div[2]/div[1]/yt-formatted-string/a"
+        )
+        await page.screenshot({"path": "./screenshot.png", "fullPage": True})
+        url = await url.getProperty("href")
         return await url.jsonValue()
-    
+
+    def playnext(self, context: commands.Context):
+        if len(self.queues[context.guild.id]) > 0:
+            context.voice_client.play(
+                discord.FFmpegPCMAudio(self.queues[context.guild.id][0]),
+                after=lambda error: self.playnext(context),
+            )
+            del self.queues[context.guild.id][0]
+
     @commands.command()
-    async def stop(
-        self,
-        context: commands.Context
-    ) -> None:
+    async def stop(self, context: commands.Context) -> None:
         if context.voice_client != None:
             if context.voice_client.is_connected():
                 context.voice_client.stop()
-                await context.send('Stopped playing', ephemeral=True)
-
+                await context.send("Stopped playing", ephemeral=True)
 
     @commands.command()
     async def playsong(
         self,
         context: commands.Context,
         song: str = commands.Option(None, description="The name of the song to play"),
-
     ) -> None:
-        
-        
-        if context.voice_client != None and context.voice_client.is_connected() and context.voice_client.channel != context.author.voice.channel:
-                await context.send(f'Already connected to {context.voice_client.channel}')
+
+        if (
+            context.voice_client != None
+            and context.voice_client.is_connected()
+            and context.voice_client.channel != context.author.voice.channel
+        ):
+            await context.send(f"Already connected to {context.voice_client.channel}")
         else:
             await context.defer()
             url = await self.getURL(song)
-            v = dict(parse.parse_qsl(parse.urlsplit(url).query))['v']
+            v = dict(parse.parse_qsl(parse.urlsplit(url).query))["v"]
             result = self.ydl.extract_info(url=v, download=False)
             # song = self.ydl.download([v])
             if context.voice_client != None and context.voice_client.is_connected():
                 vc = context.voice_client
             else:
                 vc = await context.author.voice.channel.connect()
-                vc.play(discord.FFmpegPCMAudio(result['url']))
-            await context.send(f'Playing')
+            vc.play(
+                discord.FFmpegPCMAudio(result["url"]),
+                after=lambda error: self.playnext(context),
+            )
+            vc.source = discord.PCMVolumeTransformer(vc.source)
+            await context.send(f"Playing")
+
+    @commands.command()
+    async def pause(self, context: commands.Context) -> None:
+        if context.voice_client != None and context.voice_client.is_playing():
+
+            await context.send("Paused", ephemeral=True)
+
+    @commands.command()
+    async def resume(self, context: commands.Context) -> None:
+        if context.voice_client != None and context.voice_client.is_paused():
+            context.voice_client.resume()
+            await context.send("Resumed", ephemeral=True)
+
+    @commands.command()
+    async def volume(
+        self,
+        context: commands.Context,
+        volume: int = commands.Option(
+            None, description="The volume to set the player to"
+        ),
+    ) -> None:
+        if context.voice_client != None and context.voice_client.is_connected():
+            context.voice_client.source.volume = volume / 100
+            await context.send(f"Volume set to {volume}", ephemeral=True)
+
+    @commands.command()
+    async def addtoqueue(
+        self,
+        context: commands.Context,
+        song: str = commands.Option(None, description="Ths song to add to the queue"),
+    ) -> None:
+        # Check if the bot is currently playing in the guild
+        await context.defer()
+        if not context.guild.id in self.queues:
+            self.queues[context.guild.id] = []
+        url = await self.getURL(song)
+        v = dict(parse.parse_qsl(parse.urlsplit(url).query))["v"]
+        result = self.ydl.extract_info(url=v, download=False)
+        self.queues[context.guild.id].append(result["url"])
+        await context.send(
+            f"Added {song} to the queue for {context.guild}", ephemeral=True
+        )
+
 
 def setup(bot: Bot):
     bot.add_cog(Music(bot))
--- webcomms.py
+++ webcomms.py
@@ -10,139 +10,192 @@ config = ConfigParser()
 config.read("config.ini")
 
 
-class Comms():
+class Comms:
     def __init__(self, bot: Bot):
 
         # Init socket io
         sio = socketio.AsyncClient()
 
         # Reply to pings
-        @sio.on('ping')
+        @sio.on("ping")
         async def ping():
-            await sio.emit('pong')
+            await sio.emit("pong")
 
-        @sio.on('settingsChange')
+        @sio.on("settingsChange")
         async def settingsChange(data):
             print(data)
-            if data['key'] == 'bot-prefix':
+            if data["key"] == "bot-prefix":
                 # Do stuff with data['value']
                 await self.pool.execute(
                     "UPDATE guilds SET prefix = $1 WHERE guild = $2",
-                    data['value'],
-                    int(data['guild_id']),
+                    data["value"],
+                    int(data["guild_id"]),
                 )
-                bot.prefix.update({int(data['guild_id']): data['value']})
+                bot.prefix.update({int(data["guild_id"]): data["value"]})
 
-        @sio.on('getAllCommands')
+        @sio.on("getAllCommands")
         async def getAllCommands(data):
             commands = []
             for command in bot.commands:
                 commands.append(
-                    {"cog": command.cog_name, "name": command.name, "help": command.help})
-            data['commands'] = commands
-            await sio.emit('allCommands', data)
+                    {
+                        "cog": command.cog_name,
+                        "name": command.name,
+                        "help": command.help,
+                    }
+                )
+            data["commands"] = commands
+            await sio.emit("allCommands", data)
             print("Performed command sync")
 
-        @sio.on('getDisabledCommands')
+        @sio.on("getDisabledCommands")
         async def getDisabledCommands(data):
             disabledCommands = []
             for command in bot.disabled_command:
                 disabledCommands.append({"name": command})
-            data['disabledCommands'] = disabledCommands
-            await sio.emit('disabledCommands', data)
+            data["disabledCommands"] = disabledCommands
+            await sio.emit("disabledCommands", data)
             print("Performed disabled command sync")
 
-        @sio.on('disableCommand')
+        @sio.on("disableCommand")
         async def disableCommand(data):
-            bot.disabled_command[data['name']] = True
+            bot.disabled_command[data["name"]] = True
             await self.pool.execute(
-                "INSERT INTO commands (command, disabled) VALUES ($1, $2)", data['name'], True)
+                "INSERT INTO commands (command, disabled) VALUES ($1, $2)",
+                data["name"],
+                True,
+            )
 
-        @sio.on('enableCommand')
+        @sio.on("enableCommand")
         async def enableCommand(data):
-            bot.disabled_command.pop(data['name'])
+            bot.disabled_command.pop(data["name"])
             await self.pool.execute(
-                "DELETE FROM commands WHERE command = $1", data['name'])
+                "DELETE FROM commands WHERE command = $1", data["name"]
+            )
 
-        @sio.on('updateCommands')
+        @sio.on("updateCommands")
         async def updateCommands(data):
-            for command in data['enabled']:
+            for command in data["enabled"]:
                 is_disabled = bot.disabled_command.get(command)
                 if is_disabled:
                     bot.disabled_command.pop(command)
                     await self.pool.execute(
-                        "DELETE FROM commands WHERE command = $1", command)
-            for command in data['disabled']:
+                        "DELETE FROM commands WHERE command = $1", command
+                    )
+            for command in data["disabled"]:
                 is_disabled = bot.disabled_command.get(command)
                 if not is_disabled:
                     bot.disabled_command[command] = True
                     await self.pool.execute(
-                        "INSERT INTO commands (command, disabled) VALUES ($1, $2)", command, True)
+                        "INSERT INTO commands (command, disabled) VALUES ($1, $2)",
+                        command,
+                        True,
+                    )
 
-        @sio.on('guildEnableCommands')
+        @sio.on("guildEnableCommands")
         async def guildEnableCommand(data):
             guild_commands = await self.pool.fetch(
-                'SELECT commands FROM guilds WHERE guild=$1', int(data['guild_id']))
-            if len(guild_commands) > 0:  
-                guild_commands = guild_commands[0].get('commands')
+                "SELECT commands FROM guilds WHERE guild=$1", int(data["guild_id"])
+            )
+            if len(guild_commands) > 0:
+                guild_commands = guild_commands[0].get("commands")
             else:
-                await self.pool.execute('UPDATE guilds SET commands=$1 WHERE guild=$2', data['commands'], int(data['guild_id']))
+                await self.pool.execute(
+                    "UPDATE guilds SET commands=$1 WHERE guild=$2",
+                    data["commands"],
+                    int(data["guild_id"]),
+                )
                 return
             if guild_commands != None:
-                for command in data['commands']:
+                for command in data["commands"]:
                     if command in guild_commands:
                         guild_commands.remove(command)
             await self.pool.execute(
-                'UPDATE guilds SET commands=$1 WHERE guild=$2', guild_commands, int(data['guild_id']))
+                "UPDATE guilds SET commands=$1 WHERE guild=$2",
+                guild_commands,
+                int(data["guild_id"]),
+            )
 
-        @sio.on('guildDisableCommands')
+        @sio.on("guildDisableCommands")
         async def guildDisableCommands(data):
             guild_commands = await self.pool.fetch(
-                'SELECT commands FROM guilds WHERE guild=$1', int(data['guild_id']))
-            if len(guild_commands) > 0:  
-                guild_commands = guild_commands[0].get('commands')
+                "SELECT commands FROM guilds WHERE guild=$1", int(data["guild_id"])
+            )
+            if len(guild_commands) > 0:
+                guild_commands = guild_commands[0].get("commands")
             else:
-                await self.pool.execute('UPDATE guilds SET commands=$1 WHERE guild=$2', data['commands'], int(data['guild_id']))
+                await self.pool.execute(
+                    "UPDATE guilds SET commands=$1 WHERE guild=$2",
+                    data["commands"],
+                    int(data["guild_id"]),
+                )
                 return
 
             if guild_commands != None:
-                for command in data['commands']:
+                for command in data["commands"]:
                     if not command in guild_commands:
                         guild_commands.append(command)
                 await self.pool.execute(
-                    'UPDATE guilds SET commands=$1 WHERE guild=$2', guild_commands, int(data['guild_id']))
+                    "UPDATE guilds SET commands=$1 WHERE guild=$2",
+                    guild_commands,
+                    int(data["guild_id"]),
+                )
             else:
-                await self.pool.execute('UPDATE guilds SET commands=$1 WHERE guild=$2', data['commands'], int(data['guild_id']))
+                await self.pool.execute(
+                    "UPDATE guilds SET commands=$1 WHERE guild=$2",
+                    data["commands"],
+                    int(data["guild_id"]),
+                )
 
-        @sio.on('getGuildDisabledCommands')
+        @sio.on("getGuildDisabledCommands")
         async def getGuildDisabledCommands(data):
             guild_commands = await self.pool.fetch(
-                'SELECT commands FROM guilds WHERE guild=$1', int(data['guild_id']))
-            await sio.emit('sendGuildDisabledCommands', {
-                "sid": data["sid"], "disabledCommands": guild_commands[0].get('commands')})
+                "SELECT commands FROM guilds WHERE guild=$1", int(data["guild_id"])
+            )
+            await sio.emit(
+                "sendGuildDisabledCommands",
+                {
+                    "sid": data["sid"],
+                    "disabledCommands": guild_commands[0].get("commands"),
+                },
+            )
 
-        @sio.on('embed')
+        @sio.on("embed")
         async def sendEmbed(data):
-            channel: TextChannel = bot.get_channel(int(data['embed-channel']))
-            embed: Embed = bot.embed(description=data['message-text'], color=int(data['message-color'][1::], 16))
-            if 'thumbnail' in data:
-                embed.set_thumbnail(url=data['thumbnail'])
-            if 'image' in data:
-                embed.set_image(url=data['image'])
-            if 'message-author-url' in data and 'authorIcon' in data:
-                embed.set_author(name=data['message-author'], url=data['message-author-url'], icon_url=data['authorIcon'])
-            elif 'message-author-url' in data:
-                embed.set_author(name=data['message-author'], url=data['message-author-url'])
-            elif 'message-author' in data and 'authorIcon' in data:
-                embed.set_author(name=data['message-author'], icon_url=data['authorIcon'])
-            elif 'message-author' in data:
-                embed.set_author(name=data['message-author'])
+            channel: TextChannel = bot.get_channel(int(data["embed-channel"]))
+            embed: Embed = bot.embed(
+                description=data["message-text"],
+                color=int(data["message-color"][1::], 16),
+            )
+            if "thumbnail" in data:
+                embed.set_thumbnail(url=data["thumbnail"])
+            if "image" in data:
+                embed.set_image(url=data["image"])
+            if "message-author-url" in data and "authorIcon" in data:
+                embed.set_author(
+                    name=data["message-author"],
+                    url=data["message-author-url"],
+                    icon_url=data["authorIcon"],
+                )
+            elif "message-author-url" in data:
+                embed.set_author(
+                    name=data["message-author"], url=data["message-author-url"]
+                )
+            elif "message-author" in data and "authorIcon" in data:
+                embed.set_author(
+                    name=data["message-author"], icon_url=data["authorIcon"]
+                )
+            elif "message-author" in data:
+                embed.set_author(name=data["message-author"])
             asyncio.run_coroutine_threadsafe(channel.send(embed=embed), bot.loop)
-            
+
         async def connect():
             print("running connecting method")
-            await sio.connect(config['WEBSITE']['url'], auth={
-                "key": config['WEBSITE']['key']}, transports='websocket')
+            await sio.connect(
+                config["WEBSITE"]["url"],
+                auth={"key": config["WEBSITE"]["key"]},
+                transports="websocket",
+            )
             await sio.wait()
 
         # Beginning here, this is minorly modified code from https://git.io/Jo29J
@@ -152,8 +205,7 @@ class Comms():
 
         self.loop = asyncio.new_event_loop()
         self.pool = Database(self.loop).pool
-        t = Thread(target=start_background_loop,
-                   args=(self.loop,), daemon=True)
+        t = Thread(target=start_background_loop, args=(self.loop,), daemon=True)
         t.start()
 
         task = asyncio.run_coroutine_threadsafe(connect(), self.loop)
